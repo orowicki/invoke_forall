6b6342407acc9d75fe4eb255d040607349993194
Oskar Rowicki <oskar.rowicki@gmail.com>
2025-11-21 00:16:10 +0100

    Add header-guard and function declarations

---
+++ invoke_forall.h
@@ -0,0 +1,10 @@
+#ifndef INVOKE_FORALL_H
+#define INVOKE_FORALL_H
+
+template <typename... Args> 
+constexpr auto invoke_forall(Args &&...args);
+
+template <typename T> 
+constexpr decltype(auto) protect_arg(T &&);
+
+#endif // header-guard
