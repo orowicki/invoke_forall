ed55fcb651b130c40153d3298fabbb6122640779
Adam Bęcłowicz <beclowiczadam@gmail.com>
2025-12-02 11:03:22 +0100

    Better testing script

--- zadanie4_testy/run.sh
+++ zadanie4_testy/run.sh
@@ -15,8 +15,27 @@ echo_fail() {
     echo -e "${RED}$1${ENDCOLOR}"
 }
 
-test() {
-    for test in 101 102 103 103 105 106 107 201 202 203 204 301 402 403 404 501 502 601; do
+test_negative() {
+    for test in 101 102 103 103 105 106 107 202 203 204 502; do
+        echo -n "test ${test}: "
+        
+        _=$(clang++ -Wall -Wextra -std=c++23 -O2 -DTEST_NUM=$test invoke_forall_test.cpp 2>&1)
+        if [[ $? -ne 0 ]]; then
+            echo_ok OK[CE]
+            continue
+        fi
+        
+        _=$(./a.out 2>&1)
+        if [ $? -ne 0 ]; then
+            echo_ok OK[RE]
+        else
+            echo_fail FAIL
+        fi
+    done
+}
+
+test_positive() {
+    for test in 201 301 402 403 404 501 601; do
         echo -n "test ${test}: "
         
         _=$(clang++ -Wall -Wextra -std=c++23 -O2 -DTEST_NUM=$test invoke_forall_test.cpp 2>&1)
@@ -36,11 +55,10 @@ test() {
 
 cp ../invoke_forall.h .
 
-test
+echo "Should pass:"
+test_positive
 
 echo "Should fail:"
-echo "* 101-107"
-echo "* 202-204"
-echo "* 502"
+test_negative
 
 rm *.out invoke_forall.h
